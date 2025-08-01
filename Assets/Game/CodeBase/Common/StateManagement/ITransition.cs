8a41bd1aca8c274014f3460f5fbaca3cec94c5a9
Bukvoe <>
2025-08-01 23:26:49 +0300

    Add transition types

---
+++ Assets/Game/CodeBase/Common/StateManagement/ITransition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Game.CodeBase.Common.StateManagement
+{
+    public interface ITransition
+    {
+        Type To { get; }
+
+        Func<bool> Condition { get; }
+    }
+}
---
+++ Assets/Game/CodeBase/Common/StateManagement/Transition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game.CodeBase.Common.StateManagement
+{
+    public class Transition<TTo> : ITransition where TTo : IEnterState
+    {
+        public Transition(Func<bool> condition)
+        {
+            Condition = condition;
+        }
+
+        public Type To => typeof(TTo);
+
+        public Func<bool> Condition { get; }
+    }
+}
