2206757eed6833ddd9226e8cb61ffd7a9af92087
Bukvoe <>
2025-08-02 10:13:33 +0300

    Add Nickname field to hud and manager

---
+++ Assets/Game/CodeBase/Network/CustomNetworkManager.cs
@@ -0,0 +1,9 @@
+﻿using Mirror;
+
+namespace Game.CodeBase.Network
+{
+    public class CustomNetworkManager : NetworkManager
+    {
+        public string Nickname { get; set; } = string.Empty;
+    }
+}
