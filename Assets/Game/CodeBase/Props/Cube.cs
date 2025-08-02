33dae97d9f11ca0fda9c1b1ceee324bfcb74deb9
Bukvoe <>
2025-08-02 18:44:22 +0300

    Improve visuals

--- Assets/Game/CodeBase/Props/Cube.cs
+++ Assets/Game/CodeBase/Props/Cube.cs
@@ -1,4 +1,6 @@
-﻿using Mirror;
+﻿using System.Collections;
+using DG.Tweening;
+using Mirror;
 using UnityEngine;
 
 namespace Game.CodeBase.Props
@@ -6,11 +8,34 @@ namespace Game.CodeBase.Props
     [RequireComponent(typeof(Rigidbody))]
     public class Cube : NetworkBehaviour
     {
+        private const float SpawnTweenDuration = 1f;
+        private const float DestroyDelayInSeconds = 30f;
         [field: SerializeField] public Rigidbody Rigidbody { get; private set; }
+
+        public override void OnStartServer()
+        {
+            StartCoroutine(DestroyAfterDelay(DestroyDelayInSeconds));
+        }
 
         public override void OnStartClient()
         {
             Rigidbody.isKinematic = !isServer;
+
+            transform.localScale = Vector3.zero;
+
+            transform.DOScale(Vector3.one, SpawnTweenDuration)
+                .SetEase(Ease.OutBounce);
+
+            transform.DOShakeRotation(SpawnTweenDuration, new Vector3(15f, 30f, 15f))
+                .SetEase(Ease.OutQuad);
+        }
+
+        [Server]
+        private IEnumerator DestroyAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            NetworkServer.Destroy(gameObject);
         }
     }
 }
